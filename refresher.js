3431a1544e30c18c74139b4895b2cb4e8874cbf6
Mjadixon <mjdixon@brooksschool.org>
2025-02-27 19:29:11 +0000

    complete

---
+++ refresher.js
@@ -0,0 +1,44 @@
+
+//
+function tellMeMyAge (){
+    console.log(saying + " " + name + " you are " + age)
+    }
+function makePizza(type,size){
+console.log("One "+size+type+"pizza coming up!")
+}
+console.log("hello World")
+let x = "How are you"
+console.log(x)
+let saying = "hello"
+let name = "Bob"
+console.log(saying, name)
+let age = 10
+
+console.log("Youll be "+(age+10)+" in 10 years")
+if (name=="reed"){
+    console.log("I knew it was you Reed")
+} else {
+    console.log("hey " + name + " you are not reed")
+}
+
+let iAmWearingABelt = true
+let iAmWeaingGlasses = true
+if (iAmWeaingGlasses && iAmWearingABelt){
+    console.log("most likely not mr. Gorton")
+}
+if (!iAmWeaingGlasses && iAmWearingABelt){
+    console.log("most likely mr. Gorton")
+}
+if (iAmWeaingGlasses || iAmWearingABelt){
+    console.log("some random dude")
+} 
+for (let i = 0; i < 6; i++){
+    console.log("comp sci is awesome")
+}
+console.log(tellMeMyAge)
+console.log(makePizza("meatlovers ","gigantic "));
+function difference(){
+    let z = 1
+}
+let y = 3
+console.log( y)
