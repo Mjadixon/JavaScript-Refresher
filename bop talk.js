7a94d19cec87751e6cd04ba8a675b2ab5376ca8f
Mjadixon <mjdixon@brooksschool.org>
2025-03-04 16:10:27 +0000

    not finished

---
+++ bop talk.js
@@ -0,0 +1,9 @@
+function bopTalk(message) {
+    let vowels = "aeiou"
+       message = "banana split"
+   for (i=0;i<message.length;i++){
+       if (vowels.indexOf(message[i])1=-1) {
+
+       }
+    }
+}
---
+++ deck of cards.js
@@ -0,0 +1,38 @@
+const cardType = ["Hearts", "Spades", "Diamonds", "clubs"]
+let num = 1
+while (cardType[!4]){
+for (i = 0; i < 11; i++) {
+    console.log(i + " of " + cardType[0])
+    if (i == 10) {
+        console.log("Ace" + " of " + cardType[0])
+    }
+}
+for (i = 0; i < 11, i++;) {
+    
+    console.log(i+ " of " + cardType[1])
+    if (i == 10) {
+        console.log("Ace" + " of " + cardType[1])
+    }
+    for (i = 0; i < 11, i++;) {
+    
+        console.log(num + " of " + cardType[2])
+        if (i == 10) {
+            console.log("Ace" + " of " + cardType[2])
+        }
+    }
+    for (i = 0; i < 11, i++;) {
+        
+        console.log(i + " of " + cardType[3])
+        if (i== 10) {
+            console.log("Ace" + " of " + cardType[3])
+        }
+    }
+    for (i = 0; i < 11, i++;) {
+        
+        console.log(i+ " of " + cardType[4])
+        if (i== 10) {
+            console.log("Ace" + " of " + cardType[4])
+        }
+    }    
+}
+}
