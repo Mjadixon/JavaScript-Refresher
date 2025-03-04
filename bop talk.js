8c6bb1cfe28590cf14454ea4cf63127cbc7e4db4
Mjadixon <mjdixon@brooksschool.org>
2025-03-04 16:21:40 +0000

    not finished

--- bop talk.js
+++ bop talk.js
@@ -1,9 +1,13 @@
 function bopTalk(message) {
+    let constantant = message.subString()
     let vowels = "aeiou"
        message = "banana split"
    for (i=0;i<message.length;i++){
-       if (vowels.indexOf(message[i])1=-1) {
-
+       if (vowels.indexOf(message[i])) {
+        message+=constantant
+        console.log(message+=vowels)
+       } else {
+        console.log(constantant+"op")
        }
-    }
+    } 
 }
